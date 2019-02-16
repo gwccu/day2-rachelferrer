226b8406fb9f36d6fa60476882118a7d83ae4798
rachelferrer <rferrer@beaconschool.org>
2019-02-16 17:06:43 +0000

    Wow!

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -1,1 +1,13 @@
 # file name: problemSetDay2.py
+name = input('''What is your name?''')
+print(name)
+birthday = input('''When is your birthday?''')
+print(birthday)
+grade = input('''What grade are you in?''')
+print(grade)
+school = input('''What school do you attend?''')
+print(school)
+subject = input('''What is your favorite school subject?''')
+print(subject)
+job = input('''What do you want to be when you grow up?''')
+print(job)
