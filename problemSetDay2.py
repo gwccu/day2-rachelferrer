79eecdc48968f8493a6e6351e169166357d4c28b
rachelferrer <rferrer@beaconschool.org>
2019-02-16 17:28:42 +0000

    Almost done

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -11,3 +11,8 @@ subject = input('''What is your favorite school subject?''')
 print(subject)
 job = input('''What do you want to be when you grow up?''')
 print(job)
+
+a = int(input("What is the value of a?"))
+b = int(input("What is the value of b?"))
+c = int(input("What is the value of c?"))
+print("x = [-b +/- sqrt(b^2 - 2ac)] / 2a")
