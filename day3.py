9f84aba51918fddb5ab2d721ebb13b95078eebdf
rachelferrer <rferrer@beaconschool.org>
2019-02-23 15:35:33 +0000

    Hello!

---
+++ day3.py
@@ -0,0 +1,7 @@
+# my code prints my name, then stores it as a variable which I can then add things to such as my last name and repeating it multiple times
+print("Rachel")
+myName = "Rachel"
+print(myName)
+print(myName + "Ferrer")
+print(myName*78)
+print("What comes first the 'chicken' or the 'egg'?")
